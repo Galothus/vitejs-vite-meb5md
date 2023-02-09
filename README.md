ba1ca821bf18675f19382a4dfa8e79bcafcbd08f
Galothus <siaipsauemailas@gmail.com>
2023-02-09 13:54:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-meb5md
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-meb5md)
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './assets/vue-virtual-scroller.css';
+
+import { createApp } from 'vue';
+import VirtualScroller from 'vue-virtual-scroller';
+
+import App from './App.vue';
+import router from './router';
+
+const app = createApp(App);
+app.use(router);
+app.use(VirtualScroller);
+app.mount('#app');
